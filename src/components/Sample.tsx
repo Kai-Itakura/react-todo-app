49d824acbe634150682d2ac57f97500758def08d
Kai-Itakura <itakai199969@gmail.com>
2023-05-18 16:01:51 +0900

    SassとMaterialUIをインストール

---
+++ src/components/Sample.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sample = (): JSX.Element => {
+  return (
+    <div>
+      <button className='button' onClick={() => console.log('button')}>
+        ボタン
+      </button>
+    </div>
+  )
+}
+
+export default Sample
